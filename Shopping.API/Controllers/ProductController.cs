bcc112252ae69215b940c4410bef7876e49fa6b0
rubian.ruiz <rubian.ruiz@ceiba.com.co>
2024-09-19 15:40:28 -0500

    Connected API and web Client

---
+++ Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(ILogger<ProductController> logger) : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
